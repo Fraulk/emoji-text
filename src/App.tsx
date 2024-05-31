d5ba01dad64f75860e90d1c3fb02730f6dd9fae7
f <fratulker@gmail.com>
2024-05-31 23:00:00 +0200

    init commit + functional poc

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import "./App.css";
+
+import { letters } from "./letters";
+
+type Letter = keyof typeof letters;
+
+const App = () => {
+    const [text, setText] = useState("hello");
+    const [emojis, setEmojis] = useState(["⬛", "🌸"]);
+
+    const splittedText = text.split("");
+
+    // https://discord.com/channels/549986543650078722/549986543650078725/1246131082676535347
+
+    const filterText = (e: any) => {
+        const value = e.target.value;
+        if (value.match(/[^a-z]/)) return;
+        setText(value);
+    }
+
+    const filterEmoji = (e: any) => {
+        const value = e.target.value;
+        if (value.match(/[^^\p{Emoji}]/u)) return;
+        // if (value.length > 1) return;
+        setEmojis((emojis) => {
+            const newEmojis = [...emojis];
+            newEmojis[parseInt(e.target.name)] = value;
+            return newEmojis;
+        });
+    }
+
+    return (
+        <>
+            <div>
+                <div className="emojified-text">
+                    {/* {splittedText.map((letter: any, i) =>
+                        letters[letter as Letter].map((row: any, j: number) => (
+                            <>
+                                <div key={j}>
+                                    {row.map((col: any, k: number) => (
+                                        <span key={k}>{emojis[col]}</span>
+                                    ))}
+                                    {i < splittedText.length - 1 && (
+                                        <span>{emojis[0]}</span>
+                                    )}
+                                </div>
+                            </>
+                        ))
+                    )} */}
+                    {/* 5 is the letter height */}
+                    {[...Array(5)].map((_, i) => (
+                        <div key={i}>
+                            {splittedText.map((letter: any, j) =>
+                                // 4 is the letter width
+                                [...Array(4)].map((_, k) => (
+                                    <span key={k}>
+                                        {emojis[letters[letter as Letter][i][k]]}
+                                        {k == 3 && j < splittedText.length - 1 ? emojis[0]: null}
+                                    </span>
+                                ))
+                            )}
+                        </div>
+                    ))}
+                </div>
+                <input
+                    type="text"
+                    value={text}
+                    onChange={filterText}
+                />
+                <input
+                    type="text"
+                    name="0"
+                    value={emojis[0]}
+                    onChange={filterEmoji}
+                />
+                <input
+                    type="text"
+                    name="1"
+                    value={emojis[1]}
+                    onChange={filterEmoji}
+                />
+            </div>
+        </>
+    );
+};
+
+export default App;
