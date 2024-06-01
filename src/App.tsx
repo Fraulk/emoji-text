7ae896ecd317f951b9b85f50ed3943f21aa7d3fe
f <fratulker@gmail.com>
2024-06-01 16:15:37 +0200

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
+import DiscordDefaultAvatar from "./assets/discord-default-avatar.png";
 
 import { letters } from "./letters";
 
@@ -8,8 +9,15 @@ type Letter = keyof typeof letters;
 const App = () => {
     const [text, setText] = useState("hello");
     const [emojis, setEmojis] = useState(["⬛", "🌸"]);
+    const [hasCopied, setHasCopied] = useState(false)
 
     const splittedText = text.split("");
+    const dateStringOptions: any = {
+        weekday: undefined,
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      };
 
     // https://discord.com/channels/549986543650078722/549986543650078725/1246131082676535347
 
@@ -17,7 +25,7 @@ const App = () => {
         const value = e.target.value;
         if (value.match(/[^a-zA-Z ]/)) return;
         setText(value.toLowerCase());
-    }
+    };
 
     const filterEmoji = (e: any) => {
         const value = e.target.value;
@@ -28,49 +36,110 @@ const App = () => {
             newEmojis[parseInt(e.target.name)] = value;
             return newEmojis;
         });
+    };
+
+    const copyEmojiText = () => {
+        const text = document.createElement("textarea");
+        text.value = splittedText.map((lett: any) => {
+            let letter = lett;
+            if (letter == " ") letter = "space";
+            return letters[letter as Letter]
+                .map((row) => row.map((cell) => emojis[cell]).join(""))
+                .join("");
+        }).join("\n\n");
+        document.body.appendChild(text);
+        text.select();
+        document.execCommand("copy");
+        document.body.removeChild(text);
+        setHasCopied(true)
+        setTimeout(() => {
+            setHasCopied(false)
+        }, 1000)
+    }
+
+    // thanks SO
+    const copyToClipboard = () => {
+        if (hasCopied) return;
+        setHasCopied(true)
+        const copyText = document.querySelector(".action-help") as HTMLElement
+        copyText.innerText = ""
+        const range = document.createRange();
+        range.selectNode(document.querySelector(".emojified-text")!);
+        window.getSelection()?.addRange(range);
+        document.execCommand("copy");
+        window.getSelection()?.removeAllRanges();
+        copyText.innerText = "Copy"
+        setTimeout(() => {
+            setHasCopied(false)
+        }, 2500)
     }
 
     return (
-        <>
-            <div>
-                <div className="emojified-text">
-                    {/* 5 is the letter height */}
-                    {[...Array(5)].map((_, i) => (
-                        <div key={i}>
-                            {splittedText.map((lett: any, j) => {
-                                // 4 is the letter width
-                                let letter = lett;
-                                if (letter == " ") letter = "space";
-                                return [...Array(4)].map((_, k) => (
-                                    <span key={k}>
-                                        {emojis[letters[letter as Letter][i][k]]}
-                                        {k == 3 && j < splittedText.length - 1 ? emojis[0] : null}
-                                    </span>
-                                ));
-                            })}
+        <div className="app">
+            <div className="date-separator">{new Date().toLocaleDateString(undefined, dateStringOptions)}</div>
+            <div className="gvp__guesses__item">
+                <img src={DiscordDefaultAvatar} alt="" onDragStart={(e) => e.preventDefault()} />
+                <div>
+                    <div className="gvp__guesses__item__name">You</div>
+                    <div className="gvp__guesses__item__message">
+                        <div className="emojified-text">
+                            {/* 5 is the letter height */}
+                            {[...Array(5)].map((_, i) => (
+                                <div key={i}>
+                                    {splittedText.map((lett: any, j) => {
+                                        // 4 is the letter width
+                                        let letter = lett;
+                                        if (letter == " ") letter = "space";
+                                        return [...Array(4)].map((_, k) => (
+                                            <span key={k}>
+                                                {emojis[letters[letter as Letter][i][k]]}
+                                                {k == 3 && j < splittedText.length - 1
+                                                    ? emojis[0]
+                                                    : null}
+                                            </span>
+                                        ));
+                                    })}
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
+                <div className="actions-bar" onClick={copyToClipboard}>
+                    {hasCopied ? null : (
+                        <div className="action-help">Copy</div>
+                    )}
+                    {hasCopied ? "✅" : (
+                        <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24"><path fill="currentColor" d="M3 16a1 1 0 0 1-1-1v-5a8 8 0 0 1 8-8h5a1 1 0 0 1 1 1v.5a.5.5 0 0 1-.5.5H10a6 6 0 0 0-6 6v5.5a.5.5 0 0 1-.5.5H3Z" ></path><path fill="currentColor" d="M6 18a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4v-4h-3a5 5 0 0 1-5-5V6h-4a4 4 0 0 0-4 4v8Z" ></path><path fill="currentColor" d="M21.73 12a3 3 0 0 0-.6-.88l-4.25-4.24a3 3 0 0 0-.88-.61V9a3 3 0 0 0 3 3h2.73Z" ></path></svg>
+                    )}
+                </div>
+            </div>
+            
+            <div className="inputs">
                 <input
                     type="text"
                     maxLength={8}
+                    className="text-input"
+                    placeholder="Some text..."
                     value={text}
                     onChange={filterText}
                 />
                 <input
                     type="text"
-                    name="0"
-                    value={emojis[0]}
+                    name="1"
+                    className="text-input emoji-input"
+                    value={emojis[1]}
                     onChange={filterEmoji}
                 />
                 <input
                     type="text"
-                    name="1"
-                    value={emojis[1]}
+                    name="0"
+                    className="text-input emoji-input"
+                    value={emojis[0]}
                     onChange={filterEmoji}
                 />
             </div>
-        </>
+            {/* Tips: use Windows Key + . or Command + Control + Spacebar  */}
+        </div>
     );
 };
 
